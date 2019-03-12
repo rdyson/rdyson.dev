417a0b3e9f2d01e5ea5257d33e008b27cc10772f
Rob Dyson <rdyson@hotmail.com>
2019-03-12 17:04:13 -0400

    Added now config

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,17 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people!</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>About</h1>
+    <p>About things here.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/about">Go to About</Link>
+    <Link to="/">Go to Home</Link>
   </Layout>
 )
 
-export default IndexPage
+export default AboutPage
