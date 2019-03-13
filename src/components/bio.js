b3fea8c29e54807c6d9aca5ece8c07cdcd95d08d
Rob Dyson <rdyson@hotmail.com>
2019-03-13 16:25:01 -0400

    Fixed title and bio mdx attempt

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,17 +14,16 @@ import { rhythm } from '../utils/typography'
 function Bio() {
   return (
     <StaticQuery
-      // query={bioQuery}
+      query={bioQuery}
       render={data => {
         const { author, social } = data.site.siteMetadata
         return (
           <div
             style={{
-              display: `flex`,
               marginBottom: rhythm(2.5),
             }}
           >
-            {
+            {/* {
               <Image
                 fixed={data.avatar.childImageSharp.fixed}
                 alt={author}
@@ -38,7 +37,7 @@ function Bio() {
                   borderRadius: `50%`,
                 }}
               />
-            }
+            } */}
             <p>
               Hi! I’m Rob Dyson. I’m a web developer based in Miami, FL.{' '}
               <span role="img" aria-label="smiley">
@@ -65,24 +64,24 @@ function Bio() {
   )
 }
 
-// const bioQuery = graphql`
-//   query BioQuery {
-//     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-//       childImageSharp {
-//         fixed(width: 50, height: 50) {
-//           ...GatsbyImageSharpFixed
-//         }
-//       }
-//     }
-//     site {
-//       siteMetadata {
-//         author
-//         social {
-//           twitter
-//         }
-//       }
-//     }
-//   }
-// `
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
 
 export default Bio
