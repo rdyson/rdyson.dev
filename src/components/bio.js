db4ee94197fa843381f143417c8216571039619c
Rob Dyson <rdyson@hotmail.com>
2019-03-14 14:16:10 -0400

    Changed bio to markdown, added experience and projects

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,15 +1,37 @@
-/**
- * Bio component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
+import ReactMarkdown from 'react-markdown'
 
 import { rhythm } from '../utils/typography'
+
+const intro = `
+Hi! I’m Rob Dyson. I’m a web developer based in Miami, FL. 😎
+I’ve been co-founder and CTO at a couple of startups.
+
+I’ve also been described as vegan, a bit English, and rather sarcastic. I’m into personal productivity, [effective altruism](https://www.effectivealtruism.org/articles/introduction-to-effective-altruism), lots of reading, and coffee. In a past life I was a music producer and engineer.
+
+Lately I’ve been using React, Gatsby, GraphQL, and ES6. I’m also familiar with Ruby on Rails and PHP.
+
+You can reach me at rob at rdyson.dev, and rdyson elsewhere.
+`
+
+const experience = `
+## Recent Experience
+
+* Co-founder and CTO at Skillied — marketplace for in-person classes
+* Co-founder and CTO at [Willing](https://willing.com) — online tool for creating estate planning documents
+* Y Combinator, Summer 2015 with Willing
+`
+
+const projects = `
+## Projects
+
+* [hair.dog](https://github.com/rdyson/hair.dog) — React app built with [Next.js](https://nextjs.org)
+* [Bulk macOS Messages](https://github.com/rdyson/bulk-macos-messages) — AppleScript and bash scripts to bulk send Messages/SMS on macOS
+* [Restaurants Nearby](https://github.com/rdyson/restaurants-nearby) — React app that finds specific (in my case, vegan) nearby restaurants using geolocation and a Firebase database
+* [Quick Text Image](https://github.com/rdyson/quick-text-image) — Node script that adds text to an image background for sharing (e.g. Instagram) using [Unsplash](https://unsplash.com) and [Cloudinary](https://cloudinary.com)
+`
 
 function Bio() {
   return (
@@ -38,25 +60,9 @@ function Bio() {
                 }}
               />
             } */}
-            <p>
-              Hi! I’m Rob Dyson. I’m a web developer based in Miami, FL.{' '}
-              <span role="img" aria-label="smiley">
-                😎
-              </span>
-              <br /> I’ve been co-founder and CTO at a couple of startups.
-            </p>
-            <p>
-              I’ve also been described as vegan, a bit English, and rather sarcastic. I’m into personal productivity,{' '}
-              <a
-                href="https://www.effectivealtruism.org/articles/introduction-to-effective-altruism/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                effective altruism
-              </a>
-              , lots of reading, and coffee. In a past life I was a music producer and engineer.
-            </p>
-            <p>You can reach me at rob at rdyson.dev, and rdyson elsewhere.</p>
+            <ReactMarkdown escapeHtml="false" source={intro} linkTarget="_blank" />
+            <ReactMarkdown escapeHtml="false" source={experience} linkTarget="_blank" />
+            <ReactMarkdown escapeHtml="false" source={projects} linkTarget="_blank" />
           </div>
         )
       }}
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,6 +5,10 @@ Moraga.overrideThemeStyles = () => ({
   p: {
     fontSize: `1.2rem`,
   },
+  h2: {
+    marginTop: `2.2rem`,
+    marginBottom: `1rem`,
+  },
 })
 
 const typography = new Typography(Moraga)
